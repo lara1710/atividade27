ba99d37ba64afa517034adb8e01aa15a585f2cc0
lara1710 <laraleticia1203@gmail.com>
2024-11-13 13:31:17 -0300

    atividades

---
+++ atividade27.py
@@ -0,0 +1,9 @@
+  if c == 6:
+        sans = input("Deseja remover algum convidado? (S/N)")
+
+if sans == "sim":
+if sans == "sim" or "S":
+        Rmv = input("Digite o nome do convidado:")
+        lista.remove(Rmv)
+        print(lista)
+        
